bbecd39f303b4cdec4d2bcbda4633977e0d2af8a
treeeula <67585794+treeeula@users.noreply.github.com>
2022-09-20 21:54:56 -0700

    add version with hour input

--- printer/script_with_hour_input.js
+++ printer/script_with_hour_input.js
@@ -12,10 +12,12 @@ const vipprackinput = document.getElementById("vipprackinput")
 const supporterrackinput = document.getElementById("supporterrackinput")
 const oprackinput = document.getElementById("oprackinput")
 
+const hourinput = document.getElementById("hourinput")
+
+const t = document.getElementById("total")
 const tps = document.getElementById("tps")
 const tpm = document.getElementById("tpm")
 const tph = document.getElementById("tph")
-const tifh = document.getElementById("tifh")
 const tpd = document.getElementById("tpd")
 
 const tb = document.getElementById("totalbronze")
@@ -61,37 +63,34 @@ function calcrack(d,v,vp,s,o) { // ALL INPUTS MUST BE SET
 
 const button = document.getElementById("calc")
 
+function a(b){
+    return b.toLocaleString()
+}
 
-button.onclick = () => {
-    let totalprint_hour = calcprinter(bronzeinput.value, silverinput.value, goldinput.value, vipinput.value, vippinput.value, supporterinput.value, opinput.value)
-    let totalrack_hour = calcrack(rackinput.value, viprackinput.value, vipprackinput.value, supporterrackinput.value, oprackinput.value)
+function calculate() {
+    let totalprint_hour = calcprinter(bronzeinput.value, silverinput.value, goldinput.value, vipinput.value, vippinput.value, supporterinput.value, opinput.value)*hourinput.value
+    let totalrack_hour = calcrack(rackinput.value, viprackinput.value, vipprackinput.value, supporterrackinput.value, oprackinput.value)*hourinput.value
     let total_hour = totalprint_hour+totalrack_hour
     let total_second = total_hour/3600
     let total_minute = total_hour/60
     let total_day = total_hour*24
+
+    let total = total_hour*hourinput.value
     
+    t.innerText = `${total.toLocaleString()} (${hourinput.value} hour(s))`
+    tb.innerText = `${a(calcprinter(bronzeinput.value,0,0,0,0,0,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    ts.innerText = `${a(calcprinter(0,silverinput.value,0,0,0,0,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    tg.innerText = `${a(calcprinter(0,0,goldinput.value,0,0,0,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    tv.innerText = `${a(calcprinter(0,0,0,vipinput.value,0,0,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    tvp.innerText = `${a(calcprinter(0,0,0,0,vippinput.value,0,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    tsu.innerText = `${a(calcprinter(0,0,0,0,0,supporterinput.value,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    to.innerText = `${a(calcprinter(0,0,0,0,0,0,opinput.value)*hourinput.value)} (${hourinput.value} hour(s))`
 
-    tps.innerText = total_second.toLocaleString()
-    tpm.innerText = total_minute.toLocaleString()   
-    tph.innerText = total_hour.toLocaleString()
-    tpd.innerText = total_day.toLocaleString()
-
-    tb.innerText = calcprinter(bronzeinput.value,0,0,0,0,0,0).toLocaleString()
-    ts.innerText = calcprinter(0,silverinput.value,0,0,0,0,0).toLocaleString()
-    tg.innerText = calcprinter(0,0,goldinput.value,0,0,0,0).toLocaleString()
-    tv.innerText = calcprinter(0,0,0,vipinput.value,0,0,0).toLocaleString()
-    tvp.innerText = calcprinter(0,0,0,0,vippinput.value,0,0).toLocaleString()
-    tsu.innerText = calcprinter(0,0,0,0,0,supporterinput.value,0).toLocaleString()
-    to.innerText = calcprinter(0,0,0,0,0,0,opinput.value).toLocaleString()
-
-    tr.innerText = calcrack(rackinput.value,0,0,0,0).toLocaleString()
-    tvr.innerText = calcrack(0,viprackinput.value,0,0,0).toLocaleString()
-    tvpr.innerText = calcrack(0,0,vipprackinput.value,0,0).toLocaleString()
-    tsr.innerText = calcrack(0,0,0,supporterrackinput.value,0).toLocaleString()
-    tor.innerText = calcrack(0,0,0,0,oprackinput.value).toLocaleString()
-
-    // tb.innerText = bronzeinput.value
-    // ts.innerText
-    console.log(`Total Singular: ${totalprint_hour} | Total Rack ${totalrack_hour} | Total ${total_hour}`)
-    
+    tr.innerText = `${a(calcrack(rackinput.value,0,0,0,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    tvr.innerText = `${a(calcrack(0,viprackinput.value,0,0,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    tvpr.innerText = `${a(calcrack(0,0,vipprackinput.value,0,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    tsr.innerText = `${a(calcrack(0,0,0,supporterrackinput.value,0)*hourinput.value)} (${hourinput.value} hour(s))`
+    tor.innerText = `${a(calcrack(0,0,0,0,oprackinput.value)*hourinput.value)} (${hourinput.value} hour(s))`
 }
+
+button.onclick = calculate()
