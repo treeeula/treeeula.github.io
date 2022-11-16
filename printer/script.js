9ca4324fc40dde833301356ce9a8a70bc19f657c
treeeula <67585794+treeeula@users.noreply.github.com>
2022-11-16 12:20:50 +0000

    Add supporter+

--- printer/script.js
+++ printer/script.js
@@ -1,15 +1,23 @@
+/*
+|| 11/16/2022 ~3:00 AM MST <|> Add Supporter+
+*/
+
+
+
 const bronzeinput = document.getElementById("bronzeinput")
 const silverinput = document.getElementById("silverinput")
 const goldinput = document.getElementById("goldinput")
 const vipinput = document.getElementById("vipinput")
 const vippinput = document.getElementById("vippinput")
 const supporterinput = document.getElementById("supporterinput")
+const supporterpinput = document.getElementById("supporterpinput")
 const opinput = document.getElementById("opinput")
 
 const rackinput = document.getElementById("rackinput")
 const viprackinput = document.getElementById("viprackinput")
 const vipprackinput = document.getElementById("vipprackinput")
 const supporterrackinput = document.getElementById("supporterrackinput")
+const supporterprackinput = document.getElementById("supporterprackinput")
 const oprackinput = document.getElementById("oprackinput")
 
 const tps = document.getElementById("tps")
@@ -24,47 +32,51 @@ const tg = document.getElementById("totalgold")
 const tv = document.getElementById("totalvip")
 const tvp = document.getElementById("totalvipp")
 const tsu = document.getElementById("totalsupporter")
+const tsup = document.getElementById("totalsupporterp")
 const to = document.getElementById("totaloneprint")
 
 const tr = document.getElementById("totalrack")
 const tvr = document.getElementById("totalviprack")
 const tvpr = document.getElementById("totalvipprack")
 const tsr = document.getElementById("totalsupporterrack")
+const tspr = document.getElementById("totalsupporterprack")
 const tor = document.getElementById("totaloprack")
 
 const rates = {
     bronze: 82800,
-    silver: 115200,
+    silver: 230400,
     gold: 237600,
     vip: 315000,
     vipp: 360000,
     supporter: 518400,
+    supporterp: 673920, // LOOOOL
     oneprint: 393840
 }
 
 const rackrates = {
-    rack: rates.bronze+rates.silver+rates.silver+rates.bronze+rates.gold,
-    viprack: rates.bronze+rates.silver+rates.silver+rates.bronze+rates.gold+rates.vip,
-    vipprack: rates.bronze+rates.silver+rates.silver+rates.bronze+rates.gold+rates.vip+rates.vipp-360000,
-    supporterrack: rates.bronze+rates.silver+rates.silver+rates.bronze+rates.gold+rates.vip+rates.vipp+rates.supporter-360000,
-    oneprintrack: rates.oneprint+rates.oneprint-380000
+    rack: rates.bronze+rates.silver+rates.bronze+rates.gold-75000,
+    viprack: rates.bronze+rates.silver+rates.silver+rates.bronze+rates.gold+rates.vip-95000,
+    vipprack: rates.bronze+rates.silver+rates.silver+rates.bronze+rates.gold+rates.vip+rates.vipp-150000,
+    supporterrack: rates.bronze+rates.silver+rates.silver+rates.bronze+rates.gold+rates.vip+rates.vipp+rates.supporter,
+    supporterprack: rates.bronze+rates.silver+rates.silver+rates.bronze+rates.gold+rates.vip+rates.vipp+rates.supporter+rates.supporterp-175000, // thx brank
+    oneprintrack: rates.oneprint+rates.oneprint
 
 }
 
-function calcprinter(b, s, g, v, vp, su, o) { // ALL INPUTS MUST BE SET
-    return b*rates.bronze+s*rates.silver+g*rates.gold+v*rates.vip+vp*rates.vipp+su*rates.supporter+o*rates.oneprint+0
+function calcprinter(b, s, g, v, vp, su, sup, o) { // ALL INPUTS MUST BE SET
+    return b*rates.bronze+s*rates.silver+g*rates.gold+v*rates.vip+vp*rates.vipp+su*rates.supporter+sup*rates.supporterp+o*rates.oneprint+0
 }
 
-function calcrack(d,v,vp,s,o) { // ALL INPUTS MUST BE SET
-    return d*rackrates.rack+v*rackrates.viprack+vp*rackrates.vipprack+s*rackrates.supporterrack+o*rackrates.oneprintrack
+function calcrack(d,v,vp,s,sp,o) { // ALL INPUTS MUST BE SET
+    return d*rackrates.rack+v*rackrates.viprack+vp*rackrates.vipprack+s*rackrates.supporterrack+rackrates.supporterprack*sp+o*rackrates.oneprintrack
 }
 
 const button = document.getElementById("calc")
 
 
 button.onclick = () => {
-    let totalprint_hour = calcprinter(bronzeinput.value, silverinput.value, goldinput.value, vipinput.value, vippinput.value, supporterinput.value, opinput.value)
-    let totalrack_hour = calcrack(rackinput.value, viprackinput.value, vipprackinput.value, supporterrackinput.value, oprackinput.value)
+    let totalprint_hour = calcprinter(bronzeinput.value, silverinput.value, goldinput.value, vipinput.value, vippinput.value, supporterinput.value, supporterpinput.value, opinput.value)
+    let totalrack_hour = calcrack(rackinput.value, viprackinput.value, vipprackinput.value, supporterrackinput.value, supporterprackinput.value, oprackinput.value)
     let total_hour = totalprint_hour+totalrack_hour
     let total_second = total_hour/3600
     let total_minute = total_hour/60
@@ -76,22 +88,22 @@ button.onclick = () => {
     tph.innerText = total_hour.toLocaleString()
     tpd.innerText = total_day.toLocaleString()
 
-    tb.innerText = calcprinter(bronzeinput.value,0,0,0,0,0,0).toLocaleString()
-    ts.innerText = calcprinter(0,silverinput.value,0,0,0,0,0).toLocaleString()
-    tg.innerText = calcprinter(0,0,goldinput.value,0,0,0,0).toLocaleString()
-    tv.innerText = calcprinter(0,0,0,vipinput.value,0,0,0).toLocaleString()
-    tvp.innerText = calcprinter(0,0,0,0,vippinput.value,0,0).toLocaleString()
-    tsu.innerText = calcprinter(0,0,0,0,0,supporterinput.value,0).toLocaleString()
-    to.innerText = calcprinter(0,0,0,0,0,0,opinput.value).toLocaleString()
+    tb.innerText = calcprinter(bronzeinput.value,0,0,0,0,0,0,0).toLocaleString()
+    ts.innerText = calcprinter(0,silverinput.value,0,0,0,0,0,0).toLocaleString()
+    tg.innerText = calcprinter(0,0,goldinput.value,0,0,0,0,0).toLocaleString()
+    tv.innerText = calcprinter(0,0,0,vipinput.value,0,0,0,0).toLocaleString()
+    tvp.innerText = calcprinter(0,0,0,0,vippinput.value,0,0,0).toLocaleString()
+    tsu.innerText = calcprinter(0,0,0,0,0,supporterinput.value,0,0).toLocaleString()
+    tsup.innerText = calcprinter(0,0,0,0,0,supporterpinput.value,0).toLocaleString()
+    to.innerText = calcprinter(0,0,0,0,0,0,0,opinput.value).toLocaleString()
 
-    tr.innerText = calcrack(rackinput.value,0,0,0,0).toLocaleString()
-    tvr.innerText = calcrack(0,viprackinput.value,0,0,0).toLocaleString()
-    tvpr.innerText = calcrack(0,0,vipprackinput.value,0,0).toLocaleString()
-    tsr.innerText = calcrack(0,0,0,supporterrackinput.value,0).toLocaleString()
-    tor.innerText = calcrack(0,0,0,0,oprackinput.value).toLocaleString()
+    tr.innerText = calcrack(rackinput.value,0,0,0,0,0).toLocaleString()
+    tvr.innerText = calcrack(0,viprackinput.value,0,0,0,0).toLocaleString()
+    tvpr.innerText = calcrack(0,0,vipprackinput.value,0,0,0).toLocaleString()
+    tsr.innerText = calcrack(0,0,0,supporterrackinput.value,0,0).toLocaleString()
+    tspr.innerText = calcrack(0,0,0,0,0,supporterpinput.value,0,0).toLocaleString()
+    tor.innerText = calcrack(0,0,0,0,0,oprackinput.value).toLocaleString()
 
-    // tb.innerText = bronzeinput.value
-    // ts.innerText
     console.log(`Total Singular: ${totalprint_hour} | Total Rack ${totalrack_hour} | Total ${total_hour}`)
     
 }
